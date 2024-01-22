e89b887d6feb8fdb6f0029bff39b8c6506152b17
Bruno Boschiero <bosc8088@gmail.com>
2024-01-22 22:27:48 +0100

    login page in react

---
+++ src/components/pages/Login.jsx
@@ -0,0 +1,118 @@
+import React, { useState } from 'react';
+import axios from "axios";
+
+
+export default function Example() {
+
+    const [email, setEmail] = useState("");
+    const [motDePasse, setMotDePasse] = useState("");
+ //   const [login, setLogin] = useState(false);
+
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+
+        const configuration = {
+            method: "post",
+            url: "http://localhost:3333/login",
+            data: {
+                email,
+                motDePasse
+            }
+        }
+
+        axios(configuration)
+            .then((result) => {
+                console.log(` success : ${JSON.stringify(result, null, 2)}`);;
+            })
+            .catch((error) => {
+               console.log(`error : ${JSON.stringify(error, null, 2)}`);
+            })
+
+    }
+    return (
+        <>
+            {/*
+          This example requires updating your template:
+  
+          ```
+          <html class="h-full bg-white">
+          <body class="h-full">
+          ```
+        */}
+            <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
+                <div className="sm:mx-auto sm:w-full sm:max-w-sm">
+                    <img
+                        className="mx-auto h-10 w-auto"
+                        src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
+                        alt="Your Company"
+                    />
+                    <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900" id="recup-password">
+                        Sign in to your account
+                    </h2>
+                </div>
+
+                <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
+                    <form className="space-y-6" action="#" method="POST">
+                        <div>
+                            <label htmlFor="email" className="block text-sm font-medium leading-6 text-gray-900">
+                                Email address
+                            </label>
+                            <div className="mt-2">
+                                <input
+                                    id="email"
+                                    name="email"
+                                    type="email"
+                                    value={email}
+                                    autoComplete="email"
+                                    required
+                                    className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                                    onChange={(e) =>{ setEmail(e.target.value)} }
+                                />
+                            </div>
+                        </div>
+
+                        <div>
+                            <div className="flex items-center justify-between">
+                                <label htmlFor="password" className="block text-sm font-medium leading-6 text-gray-900">
+                                    Password
+                                </label>
+                                <div className="text-sm">
+                                    <a href="#recup-password" className="font-semibold text-indigo-600 hover:text-indigo-500">
+                                        Forgot password?
+                                    </a>
+                                </div>
+                            </div>
+                            <div className="mt-2">
+                                <input
+                                    id="password"
+                                    name="password"
+                                    type="password"
+                                    value={motDePasse}
+                                    autoComplete="current-password"
+                                    required
+                                    className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                                    onChange={(e) =>{ setMotDePasse(e.target.value)} }
+                                />
+                            </div>
+                        </div>
+
+                        <div>
+                            <button
+                                type="submit"
+                                className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                                onClick={(e) => handleSubmit(e)}
+                            >
+                                Sign in
+                            </button>
+                        </div>
+                    </form>
+                    {/*   
+            <p className="mt-10 text-center text-sm text-gray-500">
+              Not a member?{' '}
+            </p> */}
+                </div>
+            </div>
+        </>
+    )
+}
