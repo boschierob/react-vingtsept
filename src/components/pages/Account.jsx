48514f6dae575ce5fc74a10f0728e4cc7eb6bce5
Bruno Boschiero <bosc8088@gmail.com>
2024-01-27 22:29:49 +0100

    added logout

--- src/components/pages/Account.jsx
+++ src/components/pages/Account.jsx
@@ -1,9 +1,51 @@
-import React from "react";
+import React, { useEffect, useState,  } from "react";
+import axios from "axios";
+import Cookies from "universal-cookie";
+const cookies = new Cookies();
+
+const token = cookies.get("TOKEN");
+
 
 export default function Account() {
+
+  const [message, setMessage] = useState("");
+
+  useEffect(() => {
+     // set configurations for the API call here
+     const configuration = {
+      method: "get",
+      url: "http://localhost:3333/auth-endpoint",
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    };
+
+    // make the API call
+    axios(configuration)
+      .then((result) => {
+        // assign the message in our result to the message we initialized above
+        setMessage(result.data.message);
+      })
+      .catch((error) => {
+        error = new Error();
+      });
+
+  }, [])
+
+  // logout
+  const logout = () => {
+    // destroy the cookie
+    cookies.remove("TOKEN", { path: "/" });
+    // redirect user to the landing page
+    window.location.href = "/";
+  }
+
   return (
     <div>
-      <h1 className="text-center">Account Component</h1>
+      <h1 className="text-center text-red-500">{message}</h1>
+      <button type="submit" className="button-logout" onClick={() => logout()}>
+   Logout
+</button>
     </div>
   );
 }
