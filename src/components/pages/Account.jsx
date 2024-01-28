9d49e6c8c1929595864ad6bb457489773acd3a97
Bruno Boschiero <bosc8088@gmail.com>
2024-01-28 01:20:54 +0100

    adtas to screens

--- src/components/pages/Account.jsx
+++ src/components/pages/Account.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState,  } from "react";
+import React, { useEffect, useState, } from "react";
 import axios from "axios";
 import Cookies from "universal-cookie";
 const cookies = new Cookies();
@@ -9,10 +9,11 @@ const token = cookies.get("TOKEN");
 export default function Account() {
 
   const [message, setMessage] = useState("");
+  const [user, setUser] = useState({});
 
   useEffect(() => {
-     // set configurations for the API call here
-     const configuration = {
+    // set configurations for the API call here
+    const configuration = {
       method: "get",
       url: "http://localhost:3333/auth-endpoint",
       headers: {
@@ -23,8 +24,10 @@ export default function Account() {
     // make the API call
     axios(configuration)
       .then((result) => {
+        console.log(`front : data are : ${JSON.stringify(result.data)}`);
         // assign the message in our result to the message we initialized above
         setMessage(result.data.message);
+        setUser(result.data);
       })
       .catch((error) => {
         error = new Error();
@@ -42,10 +45,43 @@ export default function Account() {
 
   return (
     <div>
-      <h1 className="text-center text-red-500">{message}</h1>
+      <h2 className="text-2xl font-bold leading-7 text-gray-900 sm:truncate sm:text-3xl sm:tracking-tight">{user.username}</h2>
+      <i>{message}</i>
+      <div className="mt-2 flex items-center text-sm text-gray-500">
+        <svg className="mr-1.5 h-5 w-5 flex-shrink-0 text-gray-400" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
+          <path fillRule="evenodd" d="M6 3.75A2.75 2.75 0 018.75 1h2.5A2.75 2.75 0 0114 3.75v.443c.572.055 1.14.122 1.706.2C17.053 4.582 18 5.75 18 7.07v3.469c0 1.126-.694 2.191-1.83 2.54-1.952.599-4.024.921-6.17.921s-4.219-.322-6.17-.921C2.694 12.73 2 11.665 2 10.539V7.07c0-1.321.947-2.489 2.294-2.676A41.047 41.047 0 016 4.193V3.75zm6.5 0v.325a41.622 41.622 0 00-5 0V3.75c0-.69.56-1.25 1.25-1.25h2.5c.69 0 1.25.56 1.25 1.25zM10 10a1 1 0 00-1 1v.01a1 1 0 001 1h.01a1 1 0 001-1V11a1 1 0 00-1-1H10z" clipRule="evenodd" />
+          <path d="M3 15.055v-.684c.126.053.255.1.39.142 2.092.642 4.313.987 6.61.987 2.297 0 4.518-.345 6.61-.987.135-.041.264-.089.39-.142v.684c0 1.347-.985 2.53-2.363 2.686a41.454 41.454 0 01-9.274 0C3.985 17.585 3 16.402 3 15.055z" />
+        </svg>
+        {user.email}
+      </div>
+      <div className="mt-2 flex items-center text-sm text-gray-500">
+        <svg className="mr-1.5 h-5 w-5 flex-shrink-0 text-gray-400" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
+          <path fillRule="evenodd" d="M9.69 18.933l.003.001C9.89 19.02 10 19 10 19s.11.02.308-.066l.002-.001.006-.003.018-.008a5.741 5.741 0 00.281-.14c.186-.096.446-.24.757-.433.62-.384 1.445-.966 2.274-1.765C15.302 14.988 17 12.493 17 9A7 7 0 103 9c0 3.492 1.698 5.988 3.355 7.584a13.731 13.731 0 002.273 1.765 11.842 11.842 0 00.976.544l.062.029.018.008.006.003zM10 11.25a2.25 2.25 0 100-4.5 2.25 2.25 0 000 4.5z" clipRule="evenodd" />
+        </svg>
+        {user.statut}
+      </div>
+      <div>
+        <ul className="divide-y divide-gray-100">
+       
+          {user.customers?.map((customer, index) => (
+            <li key={index} className="flex justify-between gap-x-6 py-5">
+              <div className="flex min-w-0 gap-x-4">
+                <div className="min-w-0 flex-auto">
+                  <p className="text-sm font-semibold leading-6 text-gray-900">{customer.nom}</p>
+                  <p className="mt-1 truncate text-xs leading-5 text-gray-500">{customer.telephone}</p>
+                </div>
+              </div>
+            </li>
+
+          ))
+          }
+        </ul>
+
+      </div>
+
       <button type="submit" className="button-logout" onClick={() => logout()}>
-   Logout
-</button>
+        Logout
+      </button>
     </div>
   );
 }
